b8424b683d4b4ab82c1686ea3d77575cbaf447a3
João Messias <joao.2020317758@aluno.iffar.edu.br>
2025-02-26 16:47:33 -0300

    Aula 8: Entendendo melhor Block Content

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Topic 
 
 def index(request):
     """Página principal do learning Log"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostra todods os assuntos"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request, 'learning_logs/topics.html', context )
 
 
