292ca831e8f2a40944f239f368a8019cce375cef
João Messias <joao.2020317758@aluno.iffar.edu.br>
2025-02-28 17:02:01 -0300

    Aula 9: Criando página de um tópico específico com todas as anotações.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from .models import Topic 
 
@@ -12,3 +13,10 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context )
 
 
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas"""
+    topic = Topic.objects.get(id = topic_id) 
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'learning_logs/topic.html', context)
+    
