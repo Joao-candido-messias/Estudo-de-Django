e4582df02ffe00df403a2744342b4affdf6c7bfb
João Messias <joao.2020317758@aluno.iffar.edu.br>
2025-03-05 17:10:06 -0300

    Aula 11: Formulários com Django

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Topic
 
-class TopicForm(forms.model.Form):
-    class meta:
+class TopicForm(forms.ModelForm):
+    class Meta:
         model = Topic
         fields = ['text']
         labels = {'text' : ''}
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,28 +1,27 @@
-
 from django.shortcuts import render
-from .models import Topic 
-from .form import TopicForm
+from .models import Topic
+from .forms import TopicForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 
 def index(request):
-    """Página principal do learning Log."""
+    """Página principal do Learning Log."""
     return render(request, 'learning_logs/index.html')
 
 
 def topics(request):
-    """Mostra todods os assuntos."""
+    """Mostra todos os assuntos."""
     topics = Topic.objects.order_by('date_added')
-    context = {'topics':topics}
-    return render(request, 'learning_logs/topics.html', context )
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 
 
 def topic(request, topic_id):
     """Mostra um único assunto e todas as suas entradas."""
-    topic = Topic.objects.get(id = topic_id) 
+    topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
-    context = {'topic':topic, 'entries':entries}
+    context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
 
@@ -32,9 +31,9 @@ def new_topic(request):
         form = TopicForm()
     else:
         form = TopicForm(request.POST)
-         if form.is_valid():
+        if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('topcis'))
-    context = {'form' : form}
+            return HttpResponseRedirect(reverse('topics'))
+
+    context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
-    
